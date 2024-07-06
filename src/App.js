34ee15929bd0b2368145406abb929952be207624
harshgupta9321 <112786943+harshgupta9321@users.noreply.github.com>
2024-07-06 18:51:05 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -79,7 +79,7 @@ const App = () => {
         ) : (
           <div className="w-80 flex flex-col gap-4 rounded-lg p-4">
             <h1 className="text-center leading-normal text-white font-medium text-3xl mb-6">
-              Welcome to <br /> CODE A PROGRAM
+              Welcome to <br /> myTokri
             </h1>
             {showOTP ? (
               <>
